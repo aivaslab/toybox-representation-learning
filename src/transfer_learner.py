91a086d6c6d8001293cd7f1d9bf1c48addc6dca3
sanyald <deepayan.sanyal@vanderbilt.edu>
2023-02-01 01:02:59 -0600

    simclr transfer learning to in12

--- src/transfer_learner.py
+++ src/transfer_learner.py
@@ -13,6 +13,8 @@ import cifar10_frac
 import torch.utils.data
 import torch.backends.cudnn
 
+import dataset_imagenet12
+
 cifar10_mean = (0.4920, 0.4827, 0.4468)
 cifar10_std = (0.2471, 0.2435, 0.2617)
 cifar100_mean = (0.5053, 0.4863, 0.4415)
@@ -21,6 +23,9 @@ core50_mean = (0.6000, 0.5691, 0.5418)
 core50_std = (0.2137, 0.2211, 0.2352)
 aloi_mean = (0.0638, 0.0988, 0.1158)
 aloi_std = (0.1272, 0.1681, 0.1964)
+IN12_MEAN = (0.4541, 0.4845, 0.4980)
+IN12_STD = (0.2928, 0.2738, 0.2756)
+IN12_DATA_PATH = "../../toybox_journal_experiments/data_12/IN-12/"
 
 
 def get_parser(desc):
@@ -96,7 +101,8 @@ def run_transfer_learner(args):
     featSize = network.classifier_fc.in_features
     
     if args['dataset'] == "cifar10":
-        trnsfr_classifier = nn.Sequential(nn.Linear(featSize, 10)).cuda()
+        trnsfr_classifier = nn.Sequential(nn.Linear(featSize, featSize // 2), nn.ReLU(),
+                                          nn.Linear(featSize // 2, 10)).cuda()
         trainTransform = transforms.Compose([transforms.ToPILImage(),
                                              transforms.Resize(224),
                                              transforms.RandomHorizontalFlip(p=0.5),
@@ -115,7 +121,8 @@ def run_transfer_learner(args):
         data_test = cifar10_frac.fCIFAR10(root="../data", train=False, download=True,
                                           transform=testTransform, hypertune=args['hypertune'], rng=args['rng'])
     elif args['dataset'] == "cifar100":
-        trnsfr_classifier = nn.Sequential(nn.Linear(featSize, 100)).cuda()
+        trnsfr_classifier = nn.Sequential(nn.Linear(featSize, featSize // 2), nn.ReLU(),
+                                          nn.Linear(featSize // 2, 100)).cuda()
         trainTransform = transforms.Compose([transforms.ToPILImage(),
                                              transforms.Resize(224),
                                              transforms.RandomHorizontalFlip(p=0.5),
@@ -134,7 +141,8 @@ def run_transfer_learner(args):
         data_test = cifar10_frac.fCIFAR100(root="../data", train=False, download=True,
                                            transform=testTransform, hypertune=args['hypertune'], rng=args['rng'])
     elif args["dataset"] == "core50":
-        trnsfr_classifier = nn.Sequential(nn.Linear(featSize, 10)).cuda()
+        trnsfr_classifier = nn.Sequential(nn.Linear(featSize, featSize // 2), nn.ReLU(),
+                                          nn.Linear(featSize // 2, 10)).cuda()
         trainTransform = transforms.Compose([transforms.ToPILImage(),
                                              transforms.Resize(224),
                                              transforms.RandomHorizontalFlip(p=0.5),
@@ -152,8 +160,30 @@ def run_transfer_learner(args):
                                          hypertune=args['hypertune'], rng=args['rng'])
         data_test = cifar10_frac.fCORe50(train=False, transform=testTransform, hypertune=args['hypertune'],
                                          rng=args['rng'])
+    elif args["dataset"] == "in12":
+        trnsfr_classifier = nn.Sequential(nn.Linear(featSize, featSize // 2), nn.ReLU(),
+                                          nn.Linear(featSize // 2, 12)).cuda()
+        trainTransform = transforms.Compose([transforms.ToPILImage(),
+                                             transforms.Resize(224),
+                                             transforms.RandomHorizontalFlip(p=0.5),
+                                             transforms.RandomCrop(size=224, padding=5),
+                                             transforms.ToTensor(),
+                                             transforms.Normalize(mean=IN12_MEAN, std=IN12_STD)])
+    
+        testTransform = transforms.Compose([transforms.ToPILImage(),
+                                            transforms.Resize(256),
+                                            transforms.CenterCrop(224),
+                                            transforms.ToTensor(),
+                                            transforms.Normalize(mean=IN12_MEAN, std=IN12_STD)
+                                            ])
+        trainData = dataset_imagenet12.DataLoaderGeneric(root=IN12_DATA_PATH, train=True, transform=trainTransform,
+                                                         fraction=args['fraction'], hypertune=args['hypertune'])
+
+        data_test = dataset_imagenet12.DataLoaderGeneric(root=IN12_DATA_PATH, train=False, transform=trainTransform,
+                                                         fraction=args['fraction'], hypertune=args['hypertune'])
     else:
-        trnsfr_classifier = nn.Sequential(nn.Linear(featSize, 1000)).cuda()
+        trnsfr_classifier = nn.Sequential(nn.Linear(featSize, featSize // 2), nn.ReLU(),
+                                          nn.Linear(featSize // 2, 1000)).cuda()
         trainTransform = transforms.Compose([transforms.ToPILImage(),
                                              transforms.Resize(224),
                                              transforms.RandomHorizontalFlip(p=0.5),
@@ -183,6 +213,10 @@ def run_transfer_learner(args):
     
     testDataLoader = torch.utils.data.DataLoader(data_test, batch_size=args['batch_size'], shuffle=True,
                                                  num_workers=4)
+    if True:
+        print("train data: ", len(trainData), utils.online_mean_and_sd(trainDataLoader))
+        print("train data: ", len(data_test), utils.online_mean_and_sd(testDataLoader))
+        
     optimizer = torch.optim.SGD(trnsfr_classifier.parameters(), lr=args['lr'], weight_decay=1e-6, momentum=0.9)
     if args['num_layers_frozen'] <= 3:
         optimizer.add_param_group({'params': network.backbone.layer4.parameters()})
